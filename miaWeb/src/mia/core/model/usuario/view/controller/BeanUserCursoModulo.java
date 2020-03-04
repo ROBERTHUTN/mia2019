d0d012f9d074f6547c4bf7f1001f35ffed580533
EDWIN <empinchao@utn.edu.ec>
2020-03-03 17:54:29 -0800

    cambios modulo link

--- miaWeb/src/mia/core/model/usuario/view/controller/BeanUserCursoModulo.java
+++ miaWeb/src/mia/core/model/usuario/view/controller/BeanUserCursoModulo.java
@@ -34,7 +34,10 @@ public class BeanUserCursoModulo implements Serializable {
 	private Curso curso= new Curso();
 	private Modulo modulo= new Modulo();
 	private Usuario usuario= new Usuario();
-	
+	private String direccion;
+	private String nombre;
+	private String paramet;
+
 	private String moduloRealizado;
 	
 	@Inject
@@ -186,6 +189,42 @@ public class BeanUserCursoModulo implements Serializable {
 	public void setUsuariocursoCar(UsuarioCurso usuariocursoCar) {
 		this.usuariocursoCar = usuariocursoCar;
 	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+
+	public String getParamet() {
+		return paramet;
+	}
+
+
+	public void setParamet(String paramet) {
+		this.paramet = paramet;
+	}
+
+
+
 
 
 	private List<UserCursoModuloDTO> userccursomoduloIdDto;
