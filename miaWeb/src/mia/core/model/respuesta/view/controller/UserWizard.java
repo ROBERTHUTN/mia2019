27987e5a2455154429d119e6fef576df61d41b9d
EDWIN <empinchao@utn.edu.ec>
2020-04-30 09:15:29 -0700

    cambios realizados

---
+++ miaWeb/src/mia/core/model/respuesta/view/controller/UserWizard.java
@@ -0,0 +1,62 @@
+package mia.core.model.respuesta.view.controller;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.primefaces.event.FlowEvent;
+
+import mia.core.model.reporte.User;
+
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class UserWizard implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+
+	    private User user = new User();
+	     
+	    private boolean skip;
+	     
+	    public User getUser() {
+	        return user;
+	    }
+	 
+	    public void setUser(User user) {
+	        this.user = user;
+	    }
+	     
+	    public void save() {        
+	        FacesMessage msg = new FacesMessage("Successful", "Welcome :" + user.getFirstname());
+	        FacesContext.getCurrentInstance().addMessage(null, msg);
+	    }
+	     
+	    public boolean isSkip() {
+	        return skip;
+	    }
+	 
+	    public void setSkip(boolean skip) {
+	        this.skip = skip;
+	    }
+	     
+	    public String onFlowProcess(FlowEvent event) {
+	    
+	        if(skip) {
+	            skip = false;   //reset in case user goes back
+	            return "confirm";
+	        }
+	        else {
+	            return event.getNewStep();
+	        }
+	    }
+	}
+
+	
+	
