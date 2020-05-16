6fbd36de27bde7bb8f9c2b407f63c4fa8ce51a2c
EDWIN <empinchao@utn.edu.ec>
2020-05-15 19:51:36 -0700

    variables y preguntas

--- miaEJB/ejbModule/mia/core/model/reporte/User.java
+++ miaEJB/ejbModule/mia/core/model/reporte/User.java
@@ -28,8 +28,423 @@ public class User implements Serializable {
 	    
 	    
 	    
+	    
+	    private String uno; 
+	    private String dos;
+	    private String tres;
+	    private String cuatro;
+	    private String cinco;
+	    private String seis;
+	    private String siete;
+	    private String ocho;
+	    private String nueve;
+	    private String diez;
+	    private String once;
+	    private String doce;
+	    private String trece;
+	    private String catorce;
+	    private String quince;
+	    private String diesiceis;
+	    private String diecisiete;
+	    private String dieciocho;
+	    private String diecinueve;
+	    private String veinte;
+	    private String veintiuno;
+	    private String veintidos;
+	    private String veintitres;
+	    private String veinticuatro;
+	    private String veinticinco;
+	    private String veintiseis;
+	    private String veintisiete;
+	    private String veintiocho;
+	    private String veintinueve;
+	    private String treinta;
+	    private String treintayuno;
+	    private String treintaydos;
+	    private String treintaytres;
+	    private String treintaycuatro;
+	    private String treintaycinco;
+	    private String treintayseis;
+	    private String treintaysiete;
+	    private String treintayocho;
+	    private String treintaynueve;
+	    private String cuarenta;
+	    private String cuarentayuno;
+	    private String cuarentaydos;
+	    private String cuarentaytres;
+	    private String cuarentaycuatro;
+	    private String cuarentaycinco;
+
+	    
+	    
+	    
+	    /////esto devo borrar
+	    
 	 
-	    public String getFirstname() {
+	    public String getUno() {
+			return uno;
+		}
+
+		public void setUno(String uno) {
+			this.uno = uno;
+		}
+
+		public String getDos() {
+			return dos;
+		}
+
+		public void setDos(String dos) {
+			this.dos = dos;
+		}
+
+		public String getTres() {
+			return tres;
+		}
+
+		public void setTres(String tres) {
+			this.tres = tres;
+		}
+
+		public String getCuatro() {
+			return cuatro;
+		}
+
+		public void setCuatro(String cuatro) {
+			this.cuatro = cuatro;
+		}
+
+		public String getCinco() {
+			return cinco;
+		}
+
+		public void setCinco(String cinco) {
+			this.cinco = cinco;
+		}
+
+		public String getSeis() {
+			return seis;
+		}
+
+		public void setSeis(String seis) {
+			this.seis = seis;
+		}
+
+		public String getSiete() {
+			return siete;
+		}
+
+		public void setSiete(String siete) {
+			this.siete = siete;
+		}
+
+		public String getOcho() {
+			return ocho;
+		}
+
+		public void setOcho(String ocho) {
+			this.ocho = ocho;
+		}
+
+		public String getNueve() {
+			return nueve;
+		}
+
+		public void setNueve(String nueve) {
+			this.nueve = nueve;
+		}
+
+		public String getDiez() {
+			return diez;
+		}
+
+		public void setDiez(String diez) {
+			this.diez = diez;
+		}
+
+		public String getOnce() {
+			return once;
+		}
+
+		public void setOnce(String once) {
+			this.once = once;
+		}
+
+		public String getDoce() {
+			return doce;
+		}
+
+		public void setDoce(String doce) {
+			this.doce = doce;
+		}
+
+		public String getTrece() {
+			return trece;
+		}
+
+		public void setTrece(String trece) {
+			this.trece = trece;
+		}
+
+		public String getCatorce() {
+			return catorce;
+		}
+
+		public void setCatorce(String catorce) {
+			this.catorce = catorce;
+		}
+
+		public String getQuince() {
+			return quince;
+		}
+
+		public void setQuince(String quince) {
+			this.quince = quince;
+		}
+
+		public String getDiesiceis() {
+			return diesiceis;
+		}
+
+		public void setDiesiceis(String diesiceis) {
+			this.diesiceis = diesiceis;
+		}
+
+		public String getDiecisiete() {
+			return diecisiete;
+		}
+
+		public void setDiecisiete(String diecisiete) {
+			this.diecisiete = diecisiete;
+		}
+
+		public String getDieciocho() {
+			return dieciocho;
+		}
+
+		public void setDieciocho(String dieciocho) {
+			this.dieciocho = dieciocho;
+		}
+
+		public String getDiecinueve() {
+			return diecinueve;
+		}
+
+		public void setDiecinueve(String diecinueve) {
+			this.diecinueve = diecinueve;
+		}
+
+		public String getVeinte() {
+			return veinte;
+		}
+
+		public void setVeinte(String veinte) {
+			this.veinte = veinte;
+		}
+
+		public String getVeintiuno() {
+			return veintiuno;
+		}
+
+		public void setVeintiuno(String veintiuno) {
+			this.veintiuno = veintiuno;
+		}
+
+		public String getVeintidos() {
+			return veintidos;
+		}
+
+		public void setVeintidos(String veintidos) {
+			this.veintidos = veintidos;
+		}
+
+		public String getVeintitres() {
+			return veintitres;
+		}
+
+		public void setVeintitres(String veintitres) {
+			this.veintitres = veintitres;
+		}
+
+		public String getVeinticuatro() {
+			return veinticuatro;
+		}
+
+		public void setVeinticuatro(String veinticuatro) {
+			this.veinticuatro = veinticuatro;
+		}
+
+		public String getVeinticinco() {
+			return veinticinco;
+		}
+
+		public void setVeinticinco(String veinticinco) {
+			this.veinticinco = veinticinco;
+		}
+
+		public String getVeintiseis() {
+			return veintiseis;
+		}
+
+		public void setVeintiseis(String veintiseis) {
+			this.veintiseis = veintiseis;
+		}
+
+		public String getVeintisiete() {
+			return veintisiete;
+		}
+
+		public void setVeintisiete(String veintisiete) {
+			this.veintisiete = veintisiete;
+		}
+
+		public String getVeintiocho() {
+			return veintiocho;
+		}
+
+		public void setVeintiocho(String veintiocho) {
+			this.veintiocho = veintiocho;
+		}
+
+		public String getVeintinueve() {
+			return veintinueve;
+		}
+
+		public void setVeintinueve(String veintinueve) {
+			this.veintinueve = veintinueve;
+		}
+
+		public String getTreinta() {
+			return treinta;
+		}
+
+		public void setTreinta(String treinta) {
+			this.treinta = treinta;
+		}
+
+		public String getTreintayuno() {
+			return treintayuno;
+		}
+
+		public void setTreintayuno(String treintayuno) {
+			this.treintayuno = treintayuno;
+		}
+
+		public String getTreintaydos() {
+			return treintaydos;
+		}
+
+		public void setTreintaydos(String treintaydos) {
+			this.treintaydos = treintaydos;
+		}
+
+		public String getTreintaytres() {
+			return treintaytres;
+		}
+
+		public void setTreintaytres(String treintaytres) {
+			this.treintaytres = treintaytres;
+		}
+
+		public String getTreintaycuatro() {
+			return treintaycuatro;
+		}
+
+		public void setTreintaycuatro(String treintaycuatro) {
+			this.treintaycuatro = treintaycuatro;
+		}
+
+		public String getTreintaycinco() {
+			return treintaycinco;
+		}
+
+		public void setTreintaycinco(String treintaycinco) {
+			this.treintaycinco = treintaycinco;
+		}
+
+		public String getTreintayseis() {
+			return treintayseis;
+		}
+
+		public void setTreintayseis(String treintayseis) {
+			this.treintayseis = treintayseis;
+		}
+
+		public String getTreintaysiete() {
+			return treintaysiete;
+		}
+
+		public void setTreintaysiete(String treintaysiete) {
+			this.treintaysiete = treintaysiete;
+		}
+
+		public String getTreintayocho() {
+			return treintayocho;
+		}
+
+		public void setTreintayocho(String treintayocho) {
+			this.treintayocho = treintayocho;
+		}
+
+		public String getTreintaynueve() {
+			return treintaynueve;
+		}
+
+		public void setTreintaynueve(String treintaynueve) {
+			this.treintaynueve = treintaynueve;
+		}
+
+		public String getCuarenta() {
+			return cuarenta;
+		}
+
+		public void setCuarenta(String cuarenta) {
+			this.cuarenta = cuarenta;
+		}
+
+		public String getCuarentayuno() {
+			return cuarentayuno;
+		}
+
+		public void setCuarentayuno(String cuarentayuno) {
+			this.cuarentayuno = cuarentayuno;
+		}
+
+		public String getCuarentaydos() {
+			return cuarentaydos;
+		}
+
+		public void setCuarentaydos(String cuarentaydos) {
+			this.cuarentaydos = cuarentaydos;
+		}
+
+		public String getCuarentaytres() {
+			return cuarentaytres;
+		}
+
+		public void setCuarentaytres(String cuarentaytres) {
+			this.cuarentaytres = cuarentaytres;
+		}
+
+		public String getCuarentaycuatro() {
+			return cuarentaycuatro;
+		}
+
+		public void setCuarentaycuatro(String cuarentaycuatro) {
+			this.cuarentaycuatro = cuarentaycuatro;
+		}
+
+		public String getCuarentaycinco() {
+			return cuarentaycinco;
+		}
+
+		public void setCuarentaycinco(String cuarentaycinco) {
+			this.cuarentaycinco = cuarentaycinco;
+		}
+
+		
+		
+		// borrar estas lineas
+		public String getFirstname() {
 	        return firstname;
 	    }
 	 
